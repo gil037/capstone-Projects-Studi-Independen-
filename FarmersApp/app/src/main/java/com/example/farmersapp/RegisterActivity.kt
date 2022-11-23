68a29de302f7750d46afbbb60fec5d89ca0b7a2e
ricoasmara <muhammadricoasmara31@gmail.com>
2022-11-23 22:34:45 +0700

    splashscreen

--- FarmersApp/app/src/main/java/com/example/farmersapp/RegisterActivity.kt
+++ FarmersApp/app/src/main/java/com/example/farmersapp/RegisterActivity.kt
@@ -2,8 +2,10 @@ package com.example.farmersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.farmersapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
