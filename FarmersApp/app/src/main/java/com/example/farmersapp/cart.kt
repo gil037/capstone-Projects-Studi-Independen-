8b5288dda6822bf17f598698aef892d92f499028
gil037 <dorayaki.lawersclub@gmail.com>
2022-12-03 22:34:52 +0800

    file halaman_utama.kt & cart.kt

---
+++ FarmersApp/app/src/main/java/com/example/farmersapp/cart.kt
@@ -0,0 +1,12 @@
+package com.gilang.farmers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.farmersapp.R
+
+class cart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
---
+++ FarmersApp/app/src/main/java/com/example/farmersapp/halaman_utama.kt
@@ -0,0 +1,12 @@
+package com.gilang.farmers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.farmersapp.R
+
+class halaman_utama : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_utama)
+    }
+}
