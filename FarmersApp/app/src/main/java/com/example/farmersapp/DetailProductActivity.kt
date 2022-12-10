6a7f99195dfef92d287f1b7d968eb310b4a42893
ricoasmara <muhammadricoasmara31@gmail.com>
2022-12-11 02:38:53 +0700

    update dettail

--- FarmersApp/app/src/main/java/com/example/farmersapp/DetailProductActivity.kt
+++ FarmersApp/app/src/main/java/com/example/farmersapp/DetailProductActivity.kt
@@ -2,8 +2,11 @@ package com.example.farmersapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import coil.load
 import com.bumptech.glide.Glide
+import com.example.farmersapp.databinding.ActivityDetailProductBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -13,47 +16,48 @@ import kotlinx.android.synthetic.main.activity_login.view.*
 import kotlinx.android.synthetic.main.activity_tambah_barang.*
 
 class DetailProductActivity : AppCompatActivity() {
-    private lateinit var binding:DetailProductActivity
+    private lateinit var binding:ActivityDetailProductBinding
     private var barangId=""
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding=ActivityDetailProductBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
-
-       loadBarang()
+    binding.btnPesan.setOnClickListener{
+        loadBarang()
     }
 
-    private fun loadBarangDetail() {
-        val ref=FirebaseDatabase.getInstance().getReference("Data Barang")
-        ref.child(barangId).addListenerForSingleValueEvent(object :ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                val time="${snapshot.child("time").value}"
-               val Deskripsi ="${ snapshot.child("Deskripsi").value}"
-               val Nama = "${snapshot.child("Nama").value}"
-               val Harga = "${snapshot.child("Harga").value}"
-               val Url = "${snapshot.child("Url").value}"
-
-
-
-            }
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
+
     private fun loadBarang(){
+        val curent=auth.currentUser!!.uid
+        val ref=FirebaseDatabase.getInstance()
         barangId=intent.getStringExtra("barangId")!!
         val namabar=intent.getStringExtra("nama")
         val deskripsibar=intent.getStringExtra("deskripsi")
         val hargabar=intent.getStringExtra("harga")
         val img=intent.getStringExtra("img")
 
-        tv_name.text=namabar
+        tv1.text=namabar
         tv_description.text=deskripsibar
         tv_harga.text=hargabar
         Glide.with(this).load(img).into(iv_product)
 
+        }
+    private fun getDetail(barangId:String){
+        val curent=auth.currentUser!!.uid
+            val ref=FirebaseDatabase.getInstance().getReference("Pesan").child(curent).child(barangId)
+        ref.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val barang=snapshot.getValue(ModelBarang::class.java)
+                Glide.with(this@DetailProductActivity).load(barang!!.Url).into(iv_product)
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
-}
+    }
